7978abf99f03873b1c8824e99802e4cc5e796065
jeweljose22 <117150116+jeweljose22@users.noreply.github.com>
2023-01-23 17:43:40 +0530

    Add files via upload

---
+++ pro9/pro9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+DateTime[] dob = new DateTime[2];
+TimeSpan[] ts1 = new TimeSpan[2];
+
+//DateTime dob = DateTime.Parse(Console.ReadLine());
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine(" dob of " + (i + 1));
+    dob[i] = Convert.ToDateTime(Console.ReadLine());
+    ts1[i] = DateTime.Now - dob[i];
+
+}
+for (int i = 0;i < 2; i++)
+{
+    int age = ts1[i].Days / 365;
+        Console.WriteLine("age "+ age );
+    if (age<10)
+    {
+        Console.WriteLine("KID");
+    }
+    else if (age>10 && age<30)
+    {
+        Console.WriteLine("YOUTH");
+    }
+    else if (age > 30 && age < 60)
+    {
+        Console.WriteLine("ADULT");
+    }
+    else
+    {
+        Console.WriteLine("older");
+    }
+}
